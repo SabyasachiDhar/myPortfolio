8a667b64e7bdc73ae050be047eaa6aa5158ad261
Sabyasachi Dhar <dhar.sabyasachi.vi@gmail.com>
2024-12-20 23:27:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header> <h1>Your Name</h1> <p>Your Title</p> </header>
+    </div>
+  );
+}
+
+export default App;
